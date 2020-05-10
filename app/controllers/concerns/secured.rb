98f57f11a76fda96af8ec97cd0d090c9c97da583
kenneth051 <kjdumba@gmail.com>
2020-05-10 18:58:58 +0200

    set up todo app

---
+++ app/controllers/concerns/secured.rb
@@ -0,0 +1,11 @@
+module Secured
+    extend ActiveSupport::Concern
+  
+    included do
+      before_action :logged_in
+    end
+  
+    def logged_in
+      redirect_to '/login' unless session[:userinfo].present?
+    end
+  end
