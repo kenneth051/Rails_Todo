2db96ef60e4266030177434858d81a14d65a7490
kenneth051 <kjdumba@gmail.com>
2020-05-10 18:55:45 +0200

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+    has_many :todos
+
+    def self.find_or_create(auth)
+		where(uid: auth.uid).first_or_initialize.tap do |user|
+			user.uid = auth.uid
+			user.name = auth.info.name
+			user.email = auth.info.email
+			user.image = auth.info.image
+			user.save!
+		end
+	end
+end
